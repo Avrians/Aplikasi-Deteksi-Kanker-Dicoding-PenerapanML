facc2c6183ddca33d245e44a80ca86218b8d30bd
Avrians <avriansyahb10@gmail.com>
2024-04-28 07:06:37 +0700

    detection success

---
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.dicoding.asclepius.view
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityMainBinding
+import java.io.File
+import com.yalantis.ucrop.UCrop
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+    private var croppedImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener {
+            currentImageUri?.let {
+                moveToResult()
+
+            } ?: run {
+                showToast(getString(R.string.no_image_selected))
+            }
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherGallery.launch(chooser)
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data
+            selectedImg?.let { uri ->
+                currentImageUri = uri
+                showImage()
+                startUCrop(uri)
+            } ?: showToast(getString(R.string.error_image_selected))
+        }
+    }
+
+    private fun startUCrop(sourceUri: Uri) {
+        val fileName = "cropped_image_${System.currentTimeMillis()}.jpg"
+        val destinationUri = Uri.fromFile(File(cacheDir, fileName))
+        UCrop.of(sourceUri, destinationUri)
+            .withAspectRatio(1f, 1f)
+            .withMaxResultSize(1000, 1000)
+            .start(this)
+    }
+
+    private fun showImage() {
+        currentImageUri?.let { uri ->
+            Log.d(TAG, "Displaying image: $uri")
+            binding.previewImageView.setImageURI(uri)
+        } ?: Log.d(TAG, getString(R.string.no_image_selected))
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            val resultUri = UCrop.getOutput(data!!)
+            resultUri?.let {
+                binding.previewImageView.setImageURI(resultUri)
+                croppedImageUri = resultUri
+            } ?: showToast(getString(R.string.crop_error))
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(data!!)
+            showToast("Crop error: ${cropError?.message}")
+        }
+    }
+
+    private fun analyzeImage() {
+        val intent = Intent(this, ResultActivity::class.java)
+        croppedImageUri?.let { uri ->
+            intent.putExtra(ResultActivity.IMAGE_URI, uri.toString())
+            startActivityForResult(intent, REQUEST_RESULT)
+        } ?: showToast(getString(R.string.image_classifier_failed))
+    }
+
+    private fun moveToResult() {
+        val intent = Intent(this, ResultActivity::class.java)
+        croppedImageUri?.let { uri ->
+            intent.putExtra(ResultActivity.IMAGE_URI, uri.toString())
+            startActivity(intent)
+        } ?: showToast(getString(R.string.image_classifier_failed))
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    companion object {
+        const val TAG = "ImagePicker"
+        private const val REQUEST_RESULT = 1001
+    }
+}
---
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -0,0 +1,67 @@
+package com.dicoding.asclepius.view
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityResultBinding
+import com.dicoding.asclepius.helper.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageString = intent.getStringExtra(IMAGE_URI)
+        if (imageString != null) {
+            val imageUri = Uri.parse(imageString)
+            displayImage(imageUri)
+
+            val imageClassifierHelper = ImageClassifierHelper(
+                context = this,
+                classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun onError(error: String) {
+                        Log.d(TAG, "Error: $error")
+                    }
+
+                    override fun onResults(result: List<Classifications>?, interenceTime: Long) {
+                        result?.let { showResult(it)}
+                    }
+                }
+            )
+            imageClassifierHelper.classifyStaticImage(imageUri)
+        } else {
+            Log.e(TAG, getString(R.string.no_image_selected))
+            finish()
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showResult(result: List<Classifications>) {
+        val topResult = result[0]
+        val label = topResult.categories[0].label
+        val score = topResult.categories[0].score
+
+        fun Float.formatToString(): String {
+            return String.format("%.2f%%", this * 100)
+        }
+        binding.resultText.text = "$label ${score.formatToString()}"
+    }
+
+    private fun displayImage(uri: Uri) {
+        Log.d(TAG, "Display Image: $uri")
+        binding.resultImage.setImageURI(uri)
+    }
+
+    companion object {
+        const val IMAGE_URI = "img_uri"
+        const val TAG = "imagepPicker"
+    }
+}
